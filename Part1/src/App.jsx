a1eed108d98bd7f5e684dcc9668e64ae38a5c45f
cvquitod <dquitoch@proton.me>
2025-03-22 16:47:48 -0500

    Ejercicio 1.8: unicafe, paso 3

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,57 +1,49 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const avg = (((good * 1) + (neutral * 0) + (bad * -1)) / total)
+  const positivos = (good * 100) / total
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      good: {good}
+      <br />neutral: {neutral}
+      <br />bad: {bad}
+      <br />all: {total}
+      <br />average:  {avg}
+      <br />positive: {positivos} %
+    </div>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
-  const [avg, setAvg] = useState(0)
-  const [positiv, setPositiv] = useState(0)
 
-  const handleGood = () => {
-    const updateGood = good + 1
-    setGood(updateGood)
-    const updateAll = all + 1
-    setAll(updateAll)
-    const updateAvg = (((updateGood * 1) + (neutral * 0) + (bad * -1)) / updateAll)
-    setAvg(updateAvg)
-    setPositiv((updateGood * 100) / updateAll)
-  }
-
-  const handleNeutral = () => {
-    const updateNeutral = neutral + 1
-    setNeutral(updateNeutral)
-    const updateAll = all + 1
-    setAll(updateAll)
-    const updateAvg = (((good * 1) + (updateNeutral * 0) + (bad * -1)) / updateAll)
-    setAvg(updateAvg)
-    setPositiv((good * 100) / updateAll)
-  }
-
-  const handleBad = () => {
-    const updateBad = bad + 1
-    setBad(updateBad)
-    const updateAll = all + 1
-    setAll(updateAll)
-    const updateAvg = (((good * 1) + (neutral * 0) + (updateBad * -1)) / updateAll)
-    setAvg(updateAvg)
-    setPositiv((good * 100) / updateAll)
+  const handleClick = (tipo) => {
+    if (tipo === 'good') {
+      setGood(good + 1)
+    } else if (tipo === 'neutral') {
+      setNeutral(neutral + 1)
+    } else if (tipo === 'bad') {
+      setBad(bad + 1)
+    }
+    setAll(all + 1)
   }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
-      <h2>statistics</h2>
-      good: {good}
-      <br />neutral: {neutral}
-      <br />bad: {bad}
-      <br />all: {all}
-      <br />average:  {avg}
-      <br />positive: {positiv} %
+      <button onClick={() => handleClick('good')}>good</button>
+      <button onClick={() => handleClick('neutral')}>neutral</button>
+      <button onClick={() => handleClick('bad')}>bad</button>
+
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
