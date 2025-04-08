57d5199b8a0e3dfeef5e8d6564bc1bf7fcf1859f
cvquitod <dquitoch@proton.me>
2025-04-07 21:38:05 -0500

    2.5: Modulo separado paso 10

--- Part2/src/App.jsx
+++ Part2/src/App.jsx
@@ -1,3 +1,5 @@
+import CourseList from './components/CourseList'
+
 const App = () => {
 
   const course = [
@@ -47,28 +49,5 @@ const App = () => {
 
   return <CourseList courses={course} />
 }
-
-const CourseList = ({ courses }) => {
-  return (
-    <div>
-      <h2>Web development curriculum</h2>
-      {courses.map((course) => {
-        const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-        return (
-          < div key={course.id} >
-            <h3>{course.name}</h3>
-            <ul>
-              {course.parts.map((part) => (
-                <li key={part.id}>{part.name} {part.exercises}</li>
-              ))}
-            </ul>
-            <p><strong>total of {totalExercises} exercises</strong></p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
 
 export default App
