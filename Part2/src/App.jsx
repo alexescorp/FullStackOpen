08199f1d6b89c0a56cce737c6c94b284eedbcdfb
cvquitod <dquitoch@proton.me>
2025-04-11 17:56:11 -0500

    2.10: La Agenda Telefónica Paso 5

--- Part2/src/App.jsx
+++ Part2/src/App.jsx
@@ -47,33 +47,56 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter show with: <input placeholder='Buscar ..' onChange={handleFiltroChange} />
-      </div>
+
+      <Filter handleChange={handleFiltroChange} />
 
       <h2>add a new</h2>
-      <form onSubmit={addPersons}>
-        <div>
-          name: <input value={newName} placeholder='Ingrese un nombre ..' onChange={handlePersonChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} placeholder='Ingrese un telefono ..' onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+      <PersonForm handleSubmit={addPersons} valorNombre={newName} handleNombreChange={handlePersonChange} valorNumero={newNumber} handleNumeroChange={handleNumberChange} />
 
+      <h2>Numbers</h2>
+      <Persons personasFiltradas={filtraPersonas} />
+
+    </div>
+  )
+}
+
+const Filter = ({ handleChange }) => {
+  return (
+    <div>
+      filter show with: <input placeholder='Buscar ..' onChange={handleChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({ handleSubmit, valorNombre, handleNombreChange, valorNumero, handleNumeroChange }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input value={valorNombre} placeholder='Ingrese un nombre ..' onChange={handleNombreChange} />
+      </div>
+      <div>
+        number: <input value={valorNumero} placeholder='Ingrese un telefono ..' onChange={handleNumeroChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ personasFiltradas }) => {
+  return (
+    <div>
       <ul>
-        {filtraPersonas.map((person) => (
-          <li key={person.name}>
-            {person.name} - {person.number}
+        {personasFiltradas.map((personasLista) => (
+          <li key={personasLista.name}>
+            {personasLista.name} - {personasLista.number}
           </li>
         ))}
       </ul>
     </div>
   )
 }
+
 
 export default App
